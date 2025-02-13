095ebdf3f5394d10c8f768c044e20e9451c3366a
varshinigulla <varshinigulla2233@gmail.com>
2025-02-13 14:34:16 +0530

    changes done

---
+++ script.js
@@ -0,0 +1,38 @@
+//fetching all the categories data
+
+let menuBar = document.getElementById("menuBar");
+let allCategories = document.getElementById("allCategories");
+
+const fetchCategory = async () => {
+
+    //fetching categories data from api
+
+    let mealData  = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+    let {categories} = await mealData.json();
+
+    // displaying mealRecipe container as none
+
+    let mealRecipe = document.getElementById("mealRecipe");
+    mealRecipe.style.display = "none";
+
+    let allmeals = document.getElementById("allmeals");
+
+    //getting all the catagories data using map method
+
+        categories.map(meal =>{
+            let containerMeal = document.createElement("div");
+            containerMeal.classList.add("meals");
+            containerMeal.innerHTML += `
+            <img id="category-thumb" onclick=fetchMeal("${meal.strCategory.toLowerCase()}"),allmeals.innerHTML="" src="${meal.strCategoryThumb}" alt="${meal.strCategory}">
+            <button id="category-name">${meal.strCategory}</button>
+            `
+            allCategories.append(containerMeal);
+
+            let mealName = document.getElementById("mealName");
+            mealName.innerHTML += `<li onclick=hidedata(),fetchMeal("${meal.strCategory.toLowerCase()}"),allmeals.innerHTML="" id="menu-name">${meal.strCategory}</li><hr>`
+            menuBar.appendChild(mealName);
+        });
+
+}
+
+fetchCategory();
